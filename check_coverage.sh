f2b51cf470cddd45a406e973e7f86cd0cf5c91c0
xerool <xerool@github.com>
2022-05-13 14:14:37 -0500

    coverage actions attempt 1

--- check_coverage.sh
+++ check_coverage.sh
@@ -1,7 +1,7 @@
 if busted --coverage --suppress-pending;
 then
     luacov
-    awk 't&&(--l<0){print}/Summary/{t=1;l=1}' luacov.report.out
+    awk 't&&(--l<0){print}/Summary/{t=1;l=1}/Total/{print "##[set-output name=coverage;]" int($NF)}' luacov.report.out
     rm luacov.report.out
 fi
 rm luacov.stats.out
