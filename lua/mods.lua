af2858a00ecdf44d9d52542b107cb3f0a0da8a3d
xerool <xerool@github.com>
2022-05-21 16:24:00 -0500

    Changed default provided layout
    Hopefully the provided code will be a little bit more intuitive to
    navigate and modify for template beginners.

--- lua/mods.lua
+++ lua/mods.lua
@@ -1,17 +1,37 @@
+-- require('mirin.newsyntax')()
+-- require('mirin.eases')()
+
 if not P1 or not P2 then
-	backToSongWheel('Two Player Mode Required')
+	SCREENMAN:SystemMessage('Two Player Mode Required')
+	print('Two Player Mode Required')
+	GAMESTATE:FinishSong()
+	foreground:hidden(1)
 	return
 end
 
--- judgment / combo proxies
+-- judgment proxies
 for pn = 1, 2 do
-	setupJudgeProxy(PJ[pn], P[pn]:GetChild('Judgment'), pn)
-	setupJudgeProxy(PC[pn], P[pn]:GetChild('Combo'), pn)
+	local judge = P[pn]:GetChild('Judgment')
+	PJ[pn]:SetTarget(judge)
+	PJ[pn]:xy(pn == 1 and scx-160 or scx+160, scy)
+	judge:hidden(1)
+	judge:sleep(9e9)
 end
+
+-- combo proxies
+for pn = 1, 2 do
+	local combo = P[pn]:GetChild('Combo')
+	PC[pn]:SetTarget(combo)
+	PC[pn]:xy(pn == 1 and scx-160 or scx+160, scy)
+	combo:hidden(1)
+	combo:sleep(9e9)
+end
+
 -- player proxies
 for pn = 1, #PP do
 	PP[pn]:SetTarget(P[pn])
+	P[pn]:xy(pn == 1 and scx-160 or scx+160, scy)
 	P[pn]:hidden(1)
 end
--- your code goes here here:
 
+-- your code goes here here:
