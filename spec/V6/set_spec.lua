ea12add35b51a5f16011b6323e94f35b1939ec52
Chegg <nicholespaz@gmail.com>
2022-06-10 18:17:56 -0500

    finished V6 set_spec file

--- spec/V6/set_spec.lua
+++ spec/V6/set_spec.lua
@@ -21,4 +21,29 @@ describe('set', function()
 	it('should detect missing beat', function()
 		assert.errors(function() xero.set(nil, {bumpy = 100}) end, 'invalid start beat')
 	end)
+
+	it('should detect missing mods table', function()
+		assert.errors(function() xero.set(0, nil) end, 'invalid mods table')
+	end)
+
+	it('should detect invalid mod names', function()
+		assert.errors(function() xero.set(0, {[1] = 100}) end, 'invalid mod: 1')
+	end)
+
+	it('should detect invalid mod percents', function()
+		assert.errors(function() xero.set(0, {bumpy = '100'}) end, 'bumpy has invalid percent')
+	end)
+
+	it('should detect invalid options table', function()
+		assert.errors(function() xero.set(0, {bumpy = 100}, 1) end, 'invalid options table')
+	end)
+
+	it('should detect invalid plr options', function()
+		assert.errors(function() xero.set(0, {bumpy = 100}, {plr = '1'}) end, 'invalid plr option')
+	end)
+
+	it('should not be callable after beat 0', function()
+		update()
+		assert.errors(function() xero.set(0, {bumpy = 100}) end, 'cannot call set after LoadCommand finished')
+	end)
 end)
