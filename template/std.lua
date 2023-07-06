ebbfd6f1cd422f1a11905b7e8b203334710a25cd
XeroOl <xerool@protonmail.com>
2023-07-05 22:04:44 -0500

    Completely delete stringbuilder

--- template/std.lua
+++ template/std.lua
@@ -223,27 +223,3 @@ function perframe_data_structure(comparator)
 		n = 0,
 	}, mt)
 end
-
--- the behavior of a stringbuilder
-local stringbuilder_mt = {
-	__index = {
-		-- :build() method converts a stringbuilder into a string, with optional delimiter
-		build = table.concat,
-		-- :clear() method empties the stringbuilder
-		clear = iclear,
-	},
-
-	-- calling a stringbuilder appends to it
-	__call = function(self, a)
-		table.insert(self, tostring(a))
-		return self
-	end,
-
-	-- stringbuilder can convert to a string
-	__tostring = table.concat,
-}
-
--- stringbuilder constructor
-function stringbuilder()
-	return setmetatable({}, stringbuilder_mt)
-end
